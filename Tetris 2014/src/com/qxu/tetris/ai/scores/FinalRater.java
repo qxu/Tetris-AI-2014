999b570fea7007e8d7cbf780c402319331de7e93
qxu <11jon111@gmail.com>
2014-03-21 09:47:24 -0400

    simplified FinalRater

--- Tetris 2014/src/com/qxu/tetris/ai/scores/FinalRater.java
+++ Tetris 2014/src/com/qxu/tetris/ai/scores/FinalRater.java
@@ -4,38 +4,17 @@ import com.qxu.tetris.TetrisGrid;
 
 public class FinalRater implements BoardRater {
 	public static BoardRater[] RATERS = {
-//			ConsecHorzHoles.INSTANCE,
-//			HeightAvg.INSTANCE,
 			HeightMax.INSTANCE,
-//			HeightMinMax.INSTANCE,
-			HeightVar.INSTANCE,
-//			HeightStdDev.INSTANCE,
 			SimpleHoles.INSTANCE,
 			ThreeVariance.INSTANCE,
 			TroughCount.INSTANCE,
-//			WeightedHoles.INSTANCE,
-//			RowsWithHolesInMostHoledColumn.INSTANCE,
-//			AverageSquaredTroughHeight.INSTANCE,
 			BlocksAboveHoles.INSTANCE
 	};
-
-	public double[] coefficients = {
-			0, // ConsecHorzHoles
-			-10, // HeightAvg
-			-1, // HeightMax
-			-1, // HeightMinMax
-			0, // HeightVar
-			-5, // HeightStdDev
-			-40, // SimpleHoles
-			-10, // ThreeVariance
-			-1, // Trough
-			-4, // WeightedHoles
-			-4, // RowsWithHolesInMostHoledColumn
-			-15, // AverageSquaredTroughHeight
-			-2 // BlocksAboveHoles
-	};
+	
+	private double[] coefficients;
 
 	public FinalRater() {
+		this.coefficients = new double[RATERS.length];
 	}
 
 	public FinalRater(double[] c) {
