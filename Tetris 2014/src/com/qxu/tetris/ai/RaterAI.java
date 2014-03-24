12baa1d9b94c1c5757cd87d936653a1ab71babc4
jqx <11jon111@gmail.com>
2014-03-23 23:21:18 -0400

    added search depth 2 to ai

--- Tetris 2014/src/com/qxu/tetris/ai/RaterAI.java
+++ Tetris 2014/src/com/qxu/tetris/ai/RaterAI.java
@@ -1,5 +1,6 @@
 package com.qxu.tetris.ai;
 
+import java.util.Iterator;
 import java.util.List;
 
 import com.qxu.tetris.TetrisBlock;
@@ -9,18 +10,18 @@ import com.qxu.tetris.ai.scores.FinalRater;
 
 public class RaterAI implements TetrisAI {
 	private FinalRater rater;
-	
+
 	public RaterAI(FinalRater rater) {
 		this.rater = rater;
 	}
-	
+
 	@Override
 	public AIMove getMove(TetrisGrid grid, Tetromino t, List<Tetromino> next) {
 		double bestScore = Double.NEGATIVE_INFINITY;
-		
+
 		int bestColumn = -1;
 		int bestOrientation = -1;
-		
+
 		List<TetrisBlock> blocks = t.getBlockChain();
 		for (int or = 0; or < blocks.size(); or++) {
 			TetrisBlock block = blocks.get(or);
@@ -28,19 +29,111 @@ public class RaterAI implements TetrisAI {
 			for (int c = 0; c <= maxCol; c++) {
 				int row = grid.getDropRow(c, block);
 				if (row + block.getData().getHeight() <= grid.getHeight()) {
-					TetrisGrid subGrid = new TetrisGrid(grid);
-					subGrid.addBlock(row, c, block);
-					double score = rater.rate(subGrid);
-					
-					if (score > bestScore) {
-						bestScore = score;
-						bestOrientation = or;
-						bestColumn = c;
+					TetrisGrid subGrid1 = new TetrisGrid(grid);
+					subGrid1.addBlock(row, c, block);
+
+					for (TetrisGrid subGrid2 : iterateMoves(subGrid1, next.get(0))) {
+						double score = rater.rate(subGrid2);
+						
+						if (score > bestScore) {
+							bestScore = score;
+							bestOrientation = or;
+							bestColumn = c;
+						}
 					}
 				}
 			}
 		}
-		
-		return (bestColumn >= 0) ? new AIMove(bestColumn, bestOrientation) : null;
+
+		return (bestColumn >= 0) ? new AIMove(bestColumn, bestOrientation)
+				: null;
+	}
+
+	private static Iterable<TetrisGrid> iterateMoves(TetrisGrid grid, Tetromino t) {
+		return new MoveIterable(grid, t);
+	}
+
+	private static class MoveIterable implements Iterable<TetrisGrid> {
+		private final TetrisGrid grid;
+		private final TetrisBlock firstBlock;
+
+		MoveIterable(TetrisGrid grid, Tetromino t) {
+			this.grid = grid;
+			this.firstBlock = t.getFirstRotation();
+		}
+
+		@Override
+		public Iterator<TetrisGrid> iterator() {
+			return new MoveIterator();
+		}
+
+		private class MoveIterator implements Iterator<TetrisGrid> {
+			private TetrisBlock curBlock;
+			private int c;
+			private int maxCol;
+			private int dropRow;
+
+			MoveIterator() {
+				this.curBlock = firstBlock;
+				do {
+					this.maxCol = grid.getWidth()
+							- curBlock.getData().getWidth();
+					for (this.c = 0; c <= maxCol; this.c++) {
+						this.dropRow = grid.getDropRow(c, curBlock);
+						if (dropRow + curBlock.getData().getHeight() <= grid
+								.getHeight()) {
+							return;
+						}
+					}
+					curBlock = curBlock.getNextRotation();
+				} while (!curBlock.equals(firstBlock));
+				this.curBlock = null;
+			}
+
+			private void incrementVars() {
+				this.c++;
+				while (c <= maxCol) {
+					this.dropRow = grid.getDropRow(c, curBlock);
+					if (dropRow + curBlock.getData().getHeight() <= grid
+							.getHeight())
+						return;
+					this.c++;
+				}
+				this.curBlock = curBlock.getNextRotation();
+				while (!curBlock.equals(firstBlock)) {
+					this.c = 0;
+					this.maxCol = grid.getWidth()
+							- curBlock.getData().getWidth();
+					while (c <= maxCol) {
+						this.dropRow = grid.getDropRow(c, curBlock);
+						if (dropRow + curBlock.getData().getHeight() <= grid
+								.getHeight())
+							return;
+						this.c++;
+					}
+					this.curBlock = curBlock.getNextRotation();
+				}
+				this.curBlock = null;
+			}
+
+			@Override
+			public boolean hasNext() {
+				return curBlock != null;
+			}
+
+			@Override
+			public TetrisGrid next() {
+				TetrisGrid next = new TetrisGrid(grid);
+				next.addBlock(dropRow, c, curBlock);
+
+				incrementVars();
+				return next;
+			}
+
+			@Override
+			public void remove() {
+				throw new UnsupportedOperationException();
+			}
+		}
 	}
 }
