15d823500688feae234a4b99301cff2cfd4aae04
qxu <11jon111@gmail.com>
2014-03-25 12:56:37 -0400

    added well sums

---
+++ Tetris 2014/src/com/qxu/tetris/ai/newscores/WellSums.java
@@ -0,0 +1,24 @@
+package com.qxu.tetris.ai.newscores;
+
+import com.qxu.tetris.TetrisGrid;
+
+public class WellSums {
+	public static int getWellSums(TetrisGrid grid) {
+		int nr = 0;
+		for (int j = grid.getHeight(); j > 0; j--) {
+			for (int i = 1; i <= grid.getWidth() + 1; i++) {
+				if (grid.get(j, i) && grid.get(j, i - 1) && grid.get(j, i + 1)) {
+					nr++;
+					for (int k = j + 1; k <= grid.getHeight(); k++) {
+						if (!grid.get(k, i)) {
+							nr++;
+						} else {
+							break;
+						}
+					}
+				}
+			}
+		}
+		return nr;
+	}
+}
