bf9d3e118aff516fecd9ce397ac040eab49f2189
qxu <11jon111@gmail.com>
2014-03-25 12:46:02 -0400

    added new ai

---
+++ Tetris 2014/src/com/qxu/tetris/ai/NewAI.java
@@ -0,0 +1,14 @@
+package com.qxu.tetris.ai;
+
+import java.util.List;
+
+import com.qxu.tetris.TetrisGrid;
+import com.qxu.tetris.Tetromino;
+
+public class NewAI implements TetrisAI {
+	@Override
+	public AIMove getMove(TetrisGrid grid, Tetromino t, List<Tetromino> next) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
